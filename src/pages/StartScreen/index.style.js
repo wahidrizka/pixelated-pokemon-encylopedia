26aa0254f8d7e5916d458bda9de8d96c24f7cbfc
wahidrizka <wahid.0310@students.amikom.ac.id>
2023-02-21 03:18:31 +0700

    ✨: startscreen bg

--- src/pages/StartScreen/index.style.js
+++ src/pages/StartScreen/index.style.js
@@ -1,28 +1,28 @@
 import styled from "@emotion/styled";
 
 export const Container = styled("section")({
-  // backgroundImage: "url('/static/pokemon-group.png')",
-  backgroundPosition: "center",
-  backgroundRepeat: "no-repeat",
-  minWidth: "100vw",
-  backgroundSize: "cover",
-  minHeight: "100vh",
-  display: "flex",
-  alignItems: "center",
-  textAlign: "center",
-  justifyContent: "center",
-  flexDirection: "column",
-  position: "relative",
-  padding: "1em",
+	backgroundImage: "url('/static/pokemon-group.gif')",
+	backgroundPosition: "center",
+	backgroundRepeat: "no-repeat",
+	minWidth: "100vw",
+	backgroundSize: "cover",
+	minHeight: "100vh",
+	display: "flex",
+	alignItems: "center",
+	textAlign: "center",
+	justifyContent: "center",
+	flexDirection: "column",
+	position: "relative",
+	padding: "1em",
 });
 
 export const Centering = styled("div")({
-  display: "flex",
-  alignItems: "center",
-  flexDirection: "column",
-  gap: "1em",
+	display: "flex",
+	alignItems: "center",
+	flexDirection: "column",
+	gap: "1em",
 });
 
 export const A = styled("a")({
-  textDecoration: "underline",
+	textDecoration: "underline",
 });
